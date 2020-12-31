459dc1aaff8449c9def061549dfe97d6b6d94854
MargaridaMarques <magg.marques.98@gmail.com>
2020-12-31 14:04:27 +0000

    add communication according to the defined protocol

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,12 @@ Servo servo5;
 Servo servo6;
 Servo servo7;
 
+
 int angle = 0; 
-int selected_servo = 0; 
-char data_rcvd; 
+uint8_t selected_servo = 0; 
+uint8_t addr; 
+uint8_t aux; 
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -35,135 +38,130 @@ void setup() {
   servo6.write(angle);
   servo7.write(angle);
 
-  Serial.begin(9600);       // initialize UART with baud rate of 9600 bps
+  Serial.begin(9600);  // initialize UART with baud rate of 9600 bps
 
 }
 
 void loop() {
 
-  if(Serial.available()) { // wait for buffer to be available
-    data_rcvd = Serial.read();   // read one byte from serial buffer and save to data_rcvd
+  if(Serial.available() >= 2) { // wait for buffer to have data
 
-    if(data_rcvd == '1') selected_servo = 1; 
-    else if(data_rcvd == '2') selected_servo = 2; 
-    else if(data_rcvd == '3') selected_servo = 3; 
-    else if(data_rcvd == '4') selected_servo = 4; 
-    else if(data_rcvd == '5') selected_servo = 5; 
-    else if(data_rcvd == '6') selected_servo = 6; 
-    else if(data_rcvd == '7') selected_servo = 7; 
-    else selected_servo = 0; 
+    aux = Serial.read() << 8; //read MSB (ADDR) from serial buffer
+    addr = aux >> 4;  //read MSB (ADDR) from serial buffer
+    selected_servo = Serial.read();  //read LSB (DATA) from serial buffer
 
-    switch (selected_servo) {
-    case 1:
-        for(angle = 0; angle <= 100; angle = angle+10)  
-        {                                  
-          servo1.write(angle); 
-          delay(15); 
-        } 
-    	delay(15);
-        // now scan back from 180 to 0 degrees
-        for(angle = 100; angle >= 0; angle = angle-10)    
-        {                                
-          servo1.write(angle);  
-          delay(15); 
-        } 
-        break;
-    
-    case 2:
-        for(angle = 0; angle <= 100; angle = angle+10)  
-        {                                  
-          servo2.write(angle); 
-          delay(15); 
-        } 
-    	delay(15);
-        // now scan back from 180 to 0 degrees
-        for(angle = 100; angle >= 0; angle = angle-10)    
-        {                                
-          servo2.write(angle);  
-          delay(15); 
-        } 
-        break;
+    if(addr == 0x03){ //message is to Distribution
+      switch (selected_servo) {
+        case 1:
+            for(angle = 0; angle <= 100; angle = angle+10)  
+            {                                  
+              servo1.write(angle); 
+              delay(15); 
+            } 
+          delay(15);
+            // now scan back from 180 to 0 degrees
+            for(angle = 100; angle >= 0; angle = angle-10)    
+            {                                
+              servo1.write(angle);  
+              delay(15); 
+            } 
+            break;
+        
+        case 2:
+            for(angle = 0; angle <= 100; angle = angle+10)  
+            {                                  
+              servo2.write(angle); 
+              delay(15); 
+            } 
+          delay(15);
+            // now scan back from 180 to 0 degrees
+            for(angle = 100; angle >= 0; angle = angle-10)    
+            {                                
+              servo2.write(angle);  
+              delay(15); 
+            } 
+            break;
 
-    case 3:
-        for(angle = 0; angle <= 100; angle = angle+10)  
-        {                                  
-          servo3.write(angle); 
-          delay(15); 
-        } 
-    	delay(15);
-        // now scan back from 180 to 0 degrees
-        for(angle = 100; angle >= 0; angle = angle-10)    
-        {                                
-          servo3.write(angle);  
-          delay(15); 
-        } 
-        break;   
+        case 3:
+            for(angle = 0; angle <= 100; angle = angle+10)  
+            {                                  
+              servo3.write(angle); 
+              delay(15); 
+            } 
+          delay(15);
+            // now scan back from 180 to 0 degrees
+            for(angle = 100; angle >= 0; angle = angle-10)    
+            {                                
+              servo3.write(angle);  
+              delay(15); 
+            } 
+            break;   
 
-    case 4:
-        for(angle = 0; angle <= 100; angle = angle+10)  
-        {                                  
-          servo4.write(angle); 
-          delay(15); 
-        } 
-    	delay(15);
-        // now scan back from 180 to 0 degrees
-        for(angle = 100; angle >= 0; angle = angle-10)    
-        {                                
-          servo4.write(angle);  
-          delay(15); 
-        } 
-        break;   
+        case 4:
+            for(angle = 0; angle <= 100; angle = angle+10)  
+            {                                  
+              servo4.write(angle); 
+              delay(15); 
+            } 
+          delay(15);
+            // now scan back from 180 to 0 degrees
+            for(angle = 100; angle >= 0; angle = angle-10)    
+            {                                
+              servo4.write(angle);  
+              delay(15); 
+            } 
+            break;   
 
 
-    case 5:
-        for(angle = 0; angle <= 100; angle = angle+10)  
-        {                                  
-          servo5.write(angle); 
-          delay(15); 
-        } 
-    	delay(15);
-        // now scan back from 180 to 0 degrees
-        for(angle = 100; angle >= 0; angle = angle-10)    
-        {                                
-          servo5.write(angle);  
-          delay(15); 
-        } 
-        break;   
+        case 5:
+            for(angle = 0; angle <= 100; angle = angle+10)  
+            {                                  
+              servo5.write(angle); 
+              delay(15); 
+            } 
+          delay(15);
+            // now scan back from 180 to 0 degrees
+            for(angle = 100; angle >= 0; angle = angle-10)    
+            {                                
+              servo5.write(angle);  
+              delay(15); 
+            } 
+            break;   
 
 
-    case 6:
-        for(angle = 0; angle <= 100; angle = angle+10)  
-        {                                  
-          servo6.write(angle); 
-          delay(15); 
-        } 
-    	delay(15);
-        // now scan back from 180 to 0 degrees
-        for(angle = 100; angle >= 0; angle = angle-10)    
-        {                                
-          servo6.write(angle);  
-          delay(15); 
-        } 
-        break;
+        case 6:
+            for(angle = 0; angle <= 100; angle = angle+10)  
+            {                                  
+              servo6.write(angle); 
+              delay(15); 
+            } 
+          delay(15);
+            // now scan back from 180 to 0 degrees
+            for(angle = 100; angle >= 0; angle = angle-10)    
+            {                                
+              servo6.write(angle);  
+              delay(15); 
+            } 
+            break;
 
-    case 7:
-        for(angle = 0; angle <= 100; angle = angle+10)  
-        {                                  
-          servo7.write(angle); 
-          delay(15); 
-        } 
-    	delay(15);
-        // now scan back from 180 to 0 degrees
-        for(angle = 100; angle >= 0; angle = angle-10)    
-        {                                
-          servo7.write(angle);  
-          delay(15); 
-        } 
-        break;
-   
-    default:
-      break;
+        case 7:
+            for(angle = 0; angle <= 100; angle = angle+10)  
+            {                                  
+              servo7.write(angle); 
+              delay(15); 
+            } 
+          delay(15);
+            // now scan back from 180 to 0 degrees
+            for(angle = 100; angle >= 0; angle = angle-10)    
+            {                                
+              servo7.write(angle);  
+              delay(15); 
+            } 
+            break;
+      
+        default:
+          break;
+    }
+    }  
   }
-  }
-
 }
