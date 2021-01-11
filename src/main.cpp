9f6687471254572329b8bcb951688d22fa05017d
MargaridaMarques <magg.marques.98@gmail.com>
2021-01-11 09:50:37 +0000

    Without Arduino Functions

--- src/main.cpp
+++ src/main.cpp
@@ -19,26 +19,43 @@ uint8_t addr;
 uint8_t aux; 
 
 
+
 void setup() {
-  // put your setup code here, to run once:
-  servo1.attach(1);
-  servo2.attach(2);
-  servo3.attach(3);
-  servo4.attach(4);
-  servo5.attach(5);
-  servo6.attach(6);
-  servo7.attach(7);
-  
-  servo1.write(angle);
-  servo2.write(angle);
-  servo3.write(angle);
-  servo4.write(angle);
-  servo5.write(angle);
-  servo6.write(angle);
-  servo7.write(angle);
+
+  // SERVO as OUT
+  DDRE |= 1 << PINE4; //Digital Pin 2, OC3B
+  DDRE |= 1 << PINE5; //Digital Pin 3, OC3C
+  DDRB |= 1 << PINB5; //Digital Pin 11, OC1A 
+  DDRE |= 1 << PINE3; //Digital Pin 5, OC3A
+  DDRH |= 1 << PINH3; //Digital Pin 6, OC4A
+  DDRH |= 1 << PINH4; //Digital Pin 7, OC4B
+  DDRH |= 1 << PINH5; //Digital Pin 8, OC4C
+
+  //Fast PWM (TOP: ICR3, Update on BOTTOM, TOV3 Flag on TOP)
+  TCNT3 = 0; 
+  ICR3 = 2499;
+  TCCR3A |= (1 << WGM31) | (1 << COM3A1) | (1 << COM3B1) | (1 << COM3C1); //Clear OC3A/B/C on Compare Match, set OC3A/B/C at BOTTOM (non-inverting mode)
+	TCCR3B |= (1 << WGM32) | (1 << WGM33) | (1 << CS31); 
+
+  TCNT4 = 0; 
+  ICR4 = 2499;
+  TCCR4A |= (1 << WGM41) | (1 << COM4A1) | (1 << COM4B1) | (1 << COM4C1); 
+	TCCR4B |= (1 << WGM42) | (1 << WGM43) | (1 << CS41); 
+
+  TCNT1 = 0; 
+  ICR1 = 2499;
+  TCCR1A |= (1 << WGM11) | (1 << COM1A1); 
+	TCCR1B |= (1 << WGM12) | (1 << WGM13) | (1 << CS11); 
+
+  OCR1A = 175;
+  OCR3A = 175;
+  OCR3B = 175;
+  OCR3C = 175;
+  OCR4A = 175;
+  OCR4B = 175;
+  OCR4C = 175;
 
   Serial.begin(9600);  // initialize UART with baud rate of 9600 bps
-
 }
 
 void loop() {
@@ -52,115 +69,57 @@ void loop() {
     if(addr == 0x03){ //message is to Distribution
       switch (selected_servo) {
         case 1:
-            for(angle = 0; angle <= 100; angle = angle+10)  
-            {                                  
-              servo1.write(angle); 
-              delay(15); 
-            } 
+          OCR3B = 600;
           delay(15);
-            // now scan back from 180 to 0 degrees
-            for(angle = 100; angle >= 0; angle = angle-10)    
-            {                                
-              servo1.write(angle);  
-              delay(15); 
-            } 
-            break;
+          // now scan back from 180 to 0 degrees
+          OCR3B = 175;
+          break;
         
         case 2:
-            for(angle = 0; angle <= 100; angle = angle+10)  
-            {                                  
-              servo2.write(angle); 
-              delay(15); 
-            } 
+          OCR3C = 600;
           delay(15);
-            // now scan back from 180 to 0 degrees
-            for(angle = 100; angle >= 0; angle = angle-10)    
-            {                                
-              servo2.write(angle);  
-              delay(15); 
-            } 
-            break;
+          // now scan back from 180 to 0 degrees
+          OCR3C = 175;
+          break;
 
         case 3:
-            for(angle = 0; angle <= 100; angle = angle+10)  
-            {                                  
-              servo3.write(angle); 
-              delay(15); 
-            } 
+          OCR1A = 600;
           delay(15);
-            // now scan back from 180 to 0 degrees
-            for(angle = 100; angle >= 0; angle = angle-10)    
-            {                                
-              servo3.write(angle);  
-              delay(15); 
-            } 
-            break;   
+          // now scan back from 180 to 0 degrees
+          OCR1A = 175;
+          break;
 
         case 4:
-            for(angle = 0; angle <= 100; angle = angle+10)  
-            {                                  
-              servo4.write(angle); 
-              delay(15); 
-            } 
+          OCR3A = 600;
           delay(15);
-            // now scan back from 180 to 0 degrees
-            for(angle = 100; angle >= 0; angle = angle-10)    
-            {                                
-              servo4.write(angle);  
-              delay(15); 
-            } 
-            break;   
-
+          // now scan back from 180 to 0 degrees
+          OCR3A = 175;
+          break;
 
         case 5:
-            for(angle = 0; angle <= 100; angle = angle+10)  
-            {                                  
-              servo5.write(angle); 
-              delay(15); 
-            } 
+          OCR4A = 600;
           delay(15);
-            // now scan back from 180 to 0 degrees
-            for(angle = 100; angle >= 0; angle = angle-10)    
-            {                                
-              servo5.write(angle);  
-              delay(15); 
-            } 
-            break;   
-
+          // now scan back from 180 to 0 degrees
+          OCR4B = 175;
+          break; 
 
         case 6:
-            for(angle = 0; angle <= 100; angle = angle+10)  
-            {                                  
-              servo6.write(angle); 
-              delay(15); 
-            } 
+          OCR4B = 600;
           delay(15);
-            // now scan back from 180 to 0 degrees
-            for(angle = 100; angle >= 0; angle = angle-10)    
-            {                                
-              servo6.write(angle);  
-              delay(15); 
-            } 
-            break;
+          // now scan back from 180 to 0 degrees
+          OCR4B = 175;
+          break;
 
         case 7:
-            for(angle = 0; angle <= 100; angle = angle+10)  
-            {                                  
-              servo7.write(angle); 
-              delay(15); 
-            } 
+          OCR4C = 600;
           delay(15);
-            // now scan back from 180 to 0 degrees
-            for(angle = 100; angle >= 0; angle = angle-10)    
-            {                                
-              servo7.write(angle);  
-              delay(15); 
-            } 
-            break;
+          // now scan back from 180 to 0 degrees
+          OCR4C = 175;
+          break;
       
         default:
           break;
-    }
+      }
     }  
   }
 }
