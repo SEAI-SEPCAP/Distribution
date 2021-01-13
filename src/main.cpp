e3d438cd37cf7371a61fefd38add2581dba17e28
MargaridaMarques <magg.marques.98@gmail.com>
2021-01-13 15:11:07 +0000

    add right angle and freq

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,27 @@
 
 #define FCPU 16000000ul
 
+#define PWMTOP 39999   // TIMER1 TOP for PRESC=8
+#define MAX  90
+#define ZERO  20
+#define ZERO06 100
+#define MAX06 30 
+#define ZERO07 120 
+#define MAX07 50
+#define P_FCLK 2000 // (16M/1000)/8
+
 int angle = 0; 
 uint8_t selected_servo = 0; 
 uint8_t addr; 
 uint8_t aux; 
 
-void setup() {
+int getangle(int angle){
+  return angle/180 * 2000 + 2000; 
+}
 
-  // SERVO as OUT
+void setup_servos(){
+
+    // SERVO as OUT
   DDRE |= 1 << PINE4; //Digital Pin 2, OC3B
   DDRE |= 1 << PINE5; //Digital Pin 3, OC3C
   DDRB |= 1 << PINB5; //Digital Pin 11, OC1A 
@@ -21,31 +34,47 @@ void setup() {
   DDRH |= 1 << PINH4; //Digital Pin 7, OC4B
   DDRH |= 1 << PINH5; //Digital Pin 8, OC4C
 
+}
+
+void setup_timer_servos() {
+
   //Fast PWM (TOP: ICR3, Update on BOTTOM, TOV3 Flag on TOP)
   TCNT3 = 0; 
-  ICR3 = 2499;
+  ICR3 = PWMTOP;  // TOP count for timer1 -> FPWM = FOSC/(N*(1+TOP)) with
+                 // FPWM=50 and N=8
   TCCR3A |= (1 << WGM31) | (1 << COM3A1) | (1 << COM3B1) | (1 << COM3C1); //Clear OC3A/B/C on Compare Match, set OC3A/B/C at BOTTOM (non-inverting mode)
-	TCCR3B |= (1 << WGM32) | (1 << WGM33) | (1 << CS31); 
+	TCCR3B |= (1 << WGM32) | (1 << WGM33) | (1 << CS31); // Preesc = 8
 
   TCNT4 = 0; 
-  ICR4 = 2499;
+  ICR4 = PWMTOP;
   TCCR4A |= (1 << WGM41) | (1 << COM4A1) | (1 << COM4B1) | (1 << COM4C1); 
 	TCCR4B |= (1 << WGM42) | (1 << WGM43) | (1 << CS41); 
 
   TCNT1 = 0; 
-  ICR1 = 2499;
+  ICR1 = PWMTOP;
   TCCR1A |= (1 << WGM11) | (1 << COM1A1); 
 	TCCR1B |= (1 << WGM12) | (1 << WGM13) | (1 << CS11); 
 
-  OCR1A = 175;
-  OCR3A = 175;
-  OCR3B = 175;
-  OCR3C = 175;
-  OCR4A = 175;
-  OCR4B = 175;
-  OCR4C = 175;
+  OCR1A = ZERO;
+  OCR3A = ZERO;
+  OCR3B = ZERO;
+  OCR3C = ZERO;
+  OCR4A = ZERO;
+  OCR4B = ZERO06;
+  OCR4C = ZERO07;
 
+}
+
+void setup_uart(){
   Serial.begin(9600);  // initialize UART with baud rate of 9600 bps
+}
+
+void setup(){
+ 
+  setup_servos();
+  setup_timer_servos();
+  setup_uart();
+
 }
 
 void loop() {
@@ -59,52 +88,52 @@ void loop() {
     if(addr == 0x03){ //message is to Distribution
       switch (selected_servo) {
         case 1:
-          OCR3B = 600;
-          delay(15);
+          OCR3B = getangle(MAX);
+          delay(400);
           // now scan back from 180 to 0 degrees
-          OCR3B = 175;
+          OCR3B = getangle(ZERO);
           break;
         
         case 2:
-          OCR3C = 600;
-          delay(15);
+          OCR3C = getangle(MAX);
+          delay(400);
           // now scan back from 180 to 0 degrees
-          OCR3C = 175;
+          OCR3C = getangle(ZERO);
           break;
 
         case 3:
-          OCR1A = 600;
-          delay(15);
+          OCR1A = getangle(MAX);
+          delay(400);
           // now scan back from 180 to 0 degrees
-          OCR1A = 175;
+          OCR1A = getangle(ZERO);
           break;
 
         case 4:
-          OCR3A = 600;
-          delay(15);
+          OCR3A = getangle(MAX);
+          delay(400);
           // now scan back from 180 to 0 degrees
-          OCR3A = 175;
+          OCR3A = getangle(ZERO);
           break;
 
         case 5:
-          OCR4A = 600;
-          delay(15);
+          OCR4A = getangle(MAX);
+          delay(400);
           // now scan back from 180 to 0 degrees
-          OCR4B = 175;
+          OCR4B = getangle(ZERO);
           break; 
 
         case 6:
-          OCR4B = 600;
-          delay(15);
+          OCR4B = getangle(MAX06);
+          delay(400);
           // now scan back from 180 to 0 degrees
-          OCR4B = 175;
+          OCR4B = getangle(ZERO06);
           break;
 
         case 7:
-          OCR4C = 600;
-          delay(15);
+          OCR4C = getangle(MAX07);
+          delay(400);
           // now scan back from 180 to 0 degrees
-          OCR4C = 175;
+          OCR4C = getangle(ZERO07);
           break;
       
         default:
